696feb37985bba7734c8d364e6224114a1ffb248
marcoref3at <marco_refaat@hotmail.com>
2022-07-08 20:33:09 +0200

    clean url "/" bug

--- AWS_Lambda_Version/utils/cleanUrl.js
+++ AWS_Lambda_Version/utils/cleanUrl.js
@@ -1,11 +1,12 @@
 let psl = require("psl");
 
-module.exports = cleanUrl = url => {
+module.exports = cleanUrl = (url) => {
   url = url.replace(/\s/g, "");
   url = url.split("http://").pop();
   url = url.split("https://").pop();
   url = url.split("www.").pop();
   url = url.split("http://www.").pop();
   url = url.split("https://www.").pop();
+  url = url.split("/")[0];
   return psl.get(url);
 };
