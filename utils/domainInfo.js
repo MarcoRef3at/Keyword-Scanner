23a94ec0ad58bfd5bf930e7611610f5a48b63fd9
marcoref3at <marco_refaat@hotmail.com>
2022-07-13 05:22:30 +0200

    node-iplocate

--- utils/domainInfo.js
+++ utils/domainInfo.js
@@ -1,86 +1,92 @@
 const whoiser = require("whoiser");
 const sslChecker = require("ssl-checker");
-// const ipInfo = require("ip-info-finder");
-const ipdetails = require("node-ip-details");
 const { convertIso2Code } = require("convert-country-codes");
+const dns = require("dns");
+const iplocate = require("node-iplocate");
 
 module.exports = domainInfo = async (url) => {
-  const ipInitialised = ipdetails.initialise({ ip: url });
-  try {
-    const result = await ipInitialised.allInformation();
-    // console.log(result);
-    var serverAddress = `${result.countryName}, ${result.countryCode}`;
-    var serverAddressISO = result.countryCode;
-
-    serverAddress = result.city
-      ? `${result.city}, ${convertIso2Code(result.countryCode).iso3}`
-      : "";
-
-    serverAddressISO = result.countryCode
-      ? convertIso2Code(result.countryCode).iso3
-      : "";
+  return new Promise((resolve, reject) => {
     try {
-      var domainWhois = await whoiser(url);
-      // console.log("domainWhois:", domainWhois);
+      dns.resolve4(url, async (err, addresses) => {
+        if (err) {
+          console.err("dns error", err);
+          return;
+        }
+        var ip = addresses[0];
+        var result = await iplocate(ip);
+        var serverAddress = `${result.country}, ${result.country_code}`;
+        var serverAddressISO = result.country_code;
+
+        serverAddress = result.city
+          ? `${result.city}, ${convertIso2Code(result.country_code).iso3}`
+          : "";
+
+        serverAddressISO = result.country_code
+          ? convertIso2Code(result.country_code).iso3
+          : "";
+        try {
+          var domainWhois = await whoiser(url);
+        } catch (error) {
+          console.log("domainWhois errrrror:", error);
+        }
+        // console.log("domainWhois:", domainWhois);
+        var City = "";
+        var State = "";
+        var Country = "";
+        var createdAt = "";
+        var expiresAt = "";
+        var whoIsID = "";
+
+        // Loop over the domains related to the site
+        for (const [key, value] of Object.entries(domainWhois)) {
+          // Loop over each domain keys and find the key name that contains required data
+          for (const [ky, val] of Object.entries(value)) {
+            if (ky.includes("City")) {
+              City = City == "" ? `${val}, ` : City;
+            }
+            if (ky.includes("State/Province")) {
+              State = State == "" ? `${val}, ` : State;
+            }
+            if (ky.includes("Country")) {
+              Country = Country == "" ? `${val}` : Country;
+              Country =
+                Country != "" &&
+                Country.length < 3 &&
+                convertIso2Code(Country).iso3;
+            }
+            if (ky.includes("Created Date")) {
+              createdAt = createdAt == "" ? `${val}` : createdAt;
+            }
+            if (ky.includes("Expiry Date")) {
+              expiresAt = expiresAt == "" ? `${val}` : expiresAt;
+            }
+            if (ky.includes("Registry Domain ID")) {
+              whoIsID = whoIsID == "" ? `${val}` : whoIsID;
+            }
+          }
+        }
+        try {
+          var https = await sslChecker(url);
+        } catch (error) {
+          var https = { valid: "" };
+        }
+
+        let domain = {
+          createdAt: createdAt,
+          expiresAt: expiresAt,
+          whoIsID: whoIsID,
+          location: City + State + Country,
+          locationISO: Country,
+          serverAddress,
+          serverAddressISO,
+          httpsValidation: https.valid,
+        };
+
+        resolve(domain);
+      });
     } catch (error) {
-      console.log("errrrror:", error);
+      console.log("error:", error);
+      resolve([]);
     }
-    // console.log("domainWhois:", domainWhois);
-    var City = "";
-    var State = "";
-    var Country = "";
-    var createdAt = "";
-    var expiresAt = "";
-    var whoIsID = "";
-
-    // Loop over the domains related to the site
-    for (const [key, value] of Object.entries(domainWhois)) {
-      // Loop over each domain keys and find the key name that contains required data
-      for (const [ky, val] of Object.entries(value)) {
-        if (ky.includes("City")) {
-          City = City == "" ? `${val}, ` : City;
-        }
-        if (ky.includes("State/Province")) {
-          State = State == "" ? `${val}, ` : State;
-        }
-        if (ky.includes("Country")) {
-          Country = Country == "" ? `${val}` : Country;
-          Country =
-            Country != "" &&
-            Country.length < 3 &&
-            convertIso2Code(Country).iso3;
-        }
-        if (ky.includes("Created Date")) {
-          createdAt = createdAt == "" ? `${val}` : createdAt;
-        }
-        if (ky.includes("Expiry Date")) {
-          expiresAt = expiresAt == "" ? `${val}` : expiresAt;
-        }
-        if (ky.includes("Registry Domain ID")) {
-          whoIsID = whoIsID == "" ? `${val}` : whoIsID;
-        }
-      }
-    }
-    try {
-      var https = await sslChecker(url);
-    } catch (error) {
-      var https = { valid: "" };
-    }
-
-    let domain = {
-      createdAt: createdAt,
-      expiresAt: expiresAt,
-      whoIsID: whoIsID,
-      location: City + State + Country,
-      locationISO: Country,
-      serverAddress,
-      serverAddressISO,
-      httpsValidation: https.valid
-    };
-
-    return domain;
-  } catch (error) {
-    console.log("error:", error);
-    return [];
-  }
+  });
 };
